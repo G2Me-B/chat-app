af9ffa7f266682ffb332511a0f7fd6999a25ebc4
G2Me-B <123091013+G2Me-B@users.noreply.github.com>
2025-08-27 16:45:24 +0000

    Pages setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Navbar from './components/Navbar'
-import { Routes,Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import SignUpPage from './pages/SignUpPage'
 import LoginPage from './pages/LoginPage'
@@ -9,18 +9,25 @@ import SettingsPage from './pages/SettingsPage'
 import { useAuthStore } from './store/useAuthStore'
 
 const App = () => {
-  const {authUser} = useAuthStore()
+  const { authUser, checkAuth } = useAuthStore()
+
+  useEffect(() => {
+    checkAuth()
+
+  }, [checkAuth])
+console.log(authUser)
+
   return (
     <div>
       <Navbar />
 
       <Routes>
-        <Route path='/' element={<HomePage/>} />
-        <Route path='/signup' element={<SignUpPage/>} />
-        <Route path='/login' element={<LoginPage/>} />
-        <Route path='/profile' element={<ProfilePage/>} />
-        <Route path='/settings' element={<SettingsPage/>} />
-        
+        <Route path='/' element={<HomePage />} />
+        <Route path='/signup' element={<SignUpPage />} />
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='/profile' element={<ProfilePage />} />
+        <Route path='/settings' element={<SettingsPage />} />
+
       </Routes>
     </div>
   )
--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -1,4 +1,5 @@
 import {create} from "zustand";
+import axiosInstance from "../lib/axios";
 
 
 export const useAuthStore = create((set) => ({
@@ -11,9 +12,14 @@ export const useAuthStore = create((set) => ({
 
     isCheckingAuth: async() => {
         try {
-            const res = await axiosInstance.get('/auth/me')
+            const res = await axiosInstance.get('/auth/check')
+            set({authUser: res.data.user})
+
         } catch (error) {
-            
+            console.log("Error in auth check",error.message);
+            set({authUser: null})
+        } finally{
+            set({isCheckingAuth: false})
         }
     }
 }))
