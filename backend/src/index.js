94489f508a09f910dd5d4f722c0515b901054efc
G2Me-B <123091013+G2Me-B@users.noreply.github.com>
2025-09-10 12:38:45 +0000

    project completed

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,9 +6,11 @@
  import messageRoutes from './routes/message.route.js';
  import cors from "cors"
 import { app,server,io } from './lib/socket.js';
+import path from 'path';
 
 //  App initialization
 const PORT = process.env.PORT
+const __dirname = path.resolve();
 
 // Middlewares
   app.use(express.json({limit: '10mb'}));
@@ -21,6 +23,13 @@ const PORT = process.env.PORT
  //  Routes
  app.use("/api/auth", authRoutes);
  app.use("/api/messages", messageRoutes)
+
+ if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.get("*", (req, res) => {
+res.sendFile(path.join(__dirname, "../frontend","dist","index.html"));
+  })
+ }
 
  server.listen(PORT, () => {
    console.log(`Server is running on port ${PORT}`);
